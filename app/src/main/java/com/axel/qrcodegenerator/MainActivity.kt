351b63b4ab832bdfc5a3dc31b544bfa5f3565710
axelasa <asaaxel82@gmail.com>
2021-11-08 10:11:53 +0300

    QR code generator

---
+++ app/src/main/java/com/axel/qrcodegenerator/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.axel.qrcodegenerator
+
+import android.graphics.Bitmap
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.qrcode.QRCodeWriter
+
+class MainActivity : AppCompatActivity() {
+    lateinit var image:ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        image = findViewById(R.id.imageView)
+        val content = "axel asa"
+
+        val writer = QRCodeWriter()
+        val bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, 512, 512)
+        val width = bitMatrix.width
+        val height = bitMatrix.height
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                bitmap.setPixel(x, y, if (bitMatrix.get(x, y)) Color.BLACK else Color.WHITE)
+            }
+        }
+        image.setImageBitmap(bitmap)
+        
+    }
+
+}
